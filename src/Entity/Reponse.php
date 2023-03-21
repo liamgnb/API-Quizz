cb0d0c3f4fa139288ba05bb39383c0a463bdbaf7
Liam GENEBRIER <liam.leksir@gmail.com>
2023-03-21 14:27:36 +0100

    Création de la base de données

---
+++ src/Entity/Reponse.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReponseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReponseRepository::class)]
+class Reponse
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle = null;
+
+    #[ORM\Column]
+    private ?bool $est_correcte = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function isEstCorrecte(): ?bool
+    {
+        return $this->est_correcte;
+    }
+
+    public function setEstCorrecte(bool $est_correcte): self
+    {
+        $this->est_correcte = $est_correcte;
+
+        return $this;
+    }
+}
